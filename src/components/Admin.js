574b6af03cb649f09e7c22e014f844d38f345916
yiyang <nguyiyang47@gmail.com>
2021-06-18 12:28:05 +0800

    Update grouping feature
    Properly assigned users to new groups upon button click by admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -18,60 +18,6 @@ export default function Admin() {
   const uid = firebase.auth().currentUser?.uid;
   const db = firebase.firestore();
 
-  // count total number of assigned users
-  const [totalAssigned, setTotalAssigned] = useState(0);
-  db.collection("users")
-    .where("groupAssigned", "==", true)
-    .get()
-    .then((querySnapshot) => {
-      setTotalAssigned(querySnapshot.size);
-    });
-
-  // number of available users for grouping
-  const [userCount, setUserCount] = useState(0);
-
-
-  const [currentGroup, setCurrentGroup] = useState(
-    Math.ceil(totalAssigned / 4)
-  );
-
-  function kmeans() {
-
-    let count = 0;
-    db.collection("users")
-      .where("groupAssigned", "==", false)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          if (count === 0) {
-            db.collection("groups")
-              .doc((currentGroup + 1).toString())
-              .set({ firstUser: doc.data().username }, { merge: true });
-            count++;
-          } else if (count === 1) {
-            db.collection("groups")
-              .doc((currentGroup + 1).toString())
-              .set({ secondUser: doc.data().username }, { merge: true });
-            count++;
-          } else if (count === 2) {
-            db.collection("groups")
-              .doc((currentGroup + 1).toString())
-              .set({ thirdUser: doc.data().username }, { merge: true });
-            count++;
-          } else if (count === 3) {
-            db.collection("groups")
-              .doc((currentGroup + 1).toString())
-              .set({ fourthUser: doc.data().username }, { merge: true });
-            count = 0;
-            setCurrentGroup(currentGroup + 1);
-          } 
-        });
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
-  }
   // count total number of users
   const [totalUsers, setTotalUsers] = useState(0);
   const display = () =>
@@ -85,6 +31,100 @@ export default function Admin() {
     display();
   });
 
+  // count total number of assigned users
+  const [totalAssigned, setTotalAssigned] = useState(0);
+  const assigned = () =>
+    db.collection("users")
+      .where("groupAssigned", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        setTotalAssigned(querySnapshot.size);
+      });
+    useEffect(() => {
+      assigned();
+    });
+
+
+  // number of available users for grouping
+  const [userCount, setUserCount] = useState(0);
+
+
+  const [currentGroup, setCurrentGroup] = useState(
+    Math.ceil(totalAssigned / 4)
+  );
+
+  function reset() {
+    db.collection("users")
+    .where("groupAssigned", "==", true)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        db.collection("users")
+        .doc(doc.id)
+        .update({ groupAssigned: false });
+      })
+    })
+  }
+
+  function kmeans() {
+    let assigned = 0
+    db.collection("users")
+      .where("groupAssigned", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        assigned = Math.ceil(querySnapshot.size/4);
+      });
+
+    let count = 0;
+    db.collection("users")
+      .where("groupAssigned", "==", false)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          if (count === 0) {
+            db.collection("groups")
+              .doc((assigned).toString())
+              .set({ firstUser: doc.data().username }, { merge: true });
+            db.collection("users")
+              .doc(doc.id)
+              .update({ groupAssigned: true });
+            count++;
+          } else if (count === 1) {
+            db.collection("groups")
+              .doc((assigned).toString())
+              .set({ secondUser: doc.data().username }, { merge: true });
+            db.collection("users")
+              .doc(doc.id)
+              .update({ groupAssigned: true });
+            count++;
+          } else if (count === 2) {
+            db.collection("groups")
+              .doc((assigned).toString())
+              .set({ thirdUser: doc.data().username }, { merge: true });
+            db.collection("users")
+              .doc(doc.id)
+              .update({ groupAssigned: true });
+            count++;
+          } else if (count === 3) {
+            db.collection("groups")
+              .doc((assigned).toString())
+              .set({ fourthUser: doc.data().username }, { merge: true });
+            db.collection("users")
+              .doc(doc.id)
+              .update({ groupAssigned: true });
+            count = 0;
+            assigned++
+          } 
+          console.log(assigned)
+        });
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }
+  
+
   return (
     <>
       <h1>Total users: {totalUsers}</h1>
@@ -92,6 +132,9 @@ export default function Admin() {
       <h1>Total unassigned users: {userCount}</h1>
       <button className="w-100" type="submit" onClick={kmeans}>
         Create groups
+      </button>
+      <button className="w-100" type="submit" onClick={reset}>
+        Reset group assigned
       </button>
       <div>
         <Button variant="link" onClick={handleLogout}>
