f4f33937cd58b36fd7503ada117f4482a3d0c2ab
lohdaijiu <77648872+lohdaijiu@users.noreply.github.com>
2021-06-21 00:43:59 +0800

    Added Styling for ChatRoom (Incomplete)

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import firebase from "firebase/app";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import "./Chat.css";
 
 export default function Home() {
   const { currentUser, logout } = useAuth();
