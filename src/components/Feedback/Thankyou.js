db5269c377e4d3ca5d9835abc5a8275a5a482714
yiyang <nguyiyang47@gmail.com>
2021-07-12 17:14:55 +0800

    Add style to ThankYou

--- src/components/Feedback/Thankyou.js
+++ src/components/Feedback/Thankyou.js
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 import { firebase } from "@firebase/app";
+import { Box, Container, Typography, Button } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "../Login_Reg_Home/AppBar/MainAppBar";
+import ThankYouImage from "./../Images/ThankYouImage.jpg";
 
-export default function Thankyou() {
+const styles = (theme) => ({
+  root: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: theme.spacing(5),
+
+    height: "100%",
+    align: "center",
+  },
+  button: {
+    border: "4px solid currentColor",
+    borderRadius: 0,
+    height: "auto",
+    padding: theme.spacing(2, 5),
+  },
+  link: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+  buoy: {
+    width: "50%",
+  },
+});
+
+function ThankYou(props) {
   const history = useHistory();
+  const { classes } = props;
 
   async function Home() {
     try {
@@ -14,13 +46,34 @@ export default function Thankyou() {
   }
 
   return (
-    <div>
-      <div>
-        <Button variant="link" onClick={Home}>
-          Return to HomePage
+    <>
+    <AppBar />
+      <Container className={classes.root} component="section">
+        <Typography align="center" color="textPrimary" variant="h2">
+        Thank you for your feedback!
+        </Typography>
+        <br></br>
+        <Typography align="center" color="textPrimary" variant="subtitle2">
+        We have received your feedback and we will work towards improving our application.
+        </Typography>
+        <br></br>
+        <img src={ThankYouImage} className={classes.buoy} alt="buoy" />
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          onClick={() => Home()}
+        >
+          Go back
         </Button>
-      </div>
-      <div>Thank you for your feedback!</div>
-    </div>
+      </Container>
+    </>
   );
 }
+
+ThankYou.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ThankYou);
+
